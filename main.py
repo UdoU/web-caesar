6569efc9e6db160aec92a29b802d4b618f0287f8
Lucas Brown <LucasOBrown@gmail.com>
2017-01-26 15:59:16 -0600

    added input box

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hellooo World!"
-        encryptedmessage = caesar.encrypt(message, 13)
-        self.response.write(encryptedmessage)
+        message = "Hello World!"
+        textarea = "<textarea>" + message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "<br>" +  submit + "</form>"
+        self.response.write(form)
 
 class EncryptHandler(webapp2.RequestHandler):
     def get(self):
